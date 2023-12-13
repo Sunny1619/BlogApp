5986a903550d7c747d5823f22117826c79c60e7f
rex <cse22105@iiitklayani.ac.in>
2023-12-13 17:57:24 +0530

    Final project

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BlogListView,BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from django.views.generic import TemplateView
 
 urlpatterns=[
     path('post/new/',BlogCreateView.as_view(),name='post_new'),
@@ -7,4 +8,5 @@ urlpatterns=[
     path('',BlogListView.as_view(),name='blog'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
 ]
